1d2a8fdddb0126afee6293064a308298271c11dd
kdircks <kdircks@hotmail.com>
2021-04-23 16:01:07 +0000

    Update

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -6,6 +6,9 @@ app.use(express.static('client'))
 const cors_app = require('cors');
 app.use(cors_app());
 
+const dotenv = require('dotenv')
+dotenv.config();
+
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
@@ -31,3 +34,19 @@ let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
 
+function getNLUInstance() {
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: '{apikey}',
+        }),
+        serviceUrl: '{url}',
+    });
+    return naturalLanguageUnderstanding;
+}
