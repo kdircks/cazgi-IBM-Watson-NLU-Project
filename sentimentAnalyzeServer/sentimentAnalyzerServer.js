70b1e818f400207be10ebad1b3e7e29cf0476d2d
kdircks <kdircks@hotmail.com>
2021-04-23 17:07:52 +0000

    Server and client side changes to implement the final project requirements

--- sentimentAnalyzeServer/sentimentAnalyzerServer.js
+++ sentimentAnalyzeServer/sentimentAnalyzerServer.js
@@ -1,52 +1,111 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const app = new express();
+
+
+dotenv.config();
+
+function getNLUInstance()
+{
+    let api_key = process.env.API_KEY;
+    let api_url = process.env.API_URL;
+
+    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
+    const {IamAuthenticator} = require('ibm-watson/auth');
+
+    const NaturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2020-08-01',
+        authenticator: new IamAuthenticator({
+            apikey: api_key,
+        }),
+        serviceUrl: api_url,
+    });
+    return NaturalLanguageUnderstanding;
+}
+
 
 app.use(express.static('client'))
 
 const cors_app = require('cors');
 app.use(cors_app());
 
-const dotenv = require('dotenv')
-dotenv.config();
-
 app.get("/",(req,res)=>{
     res.render('index.html');
   });
 
 app.get("/url/emotion", (req,res) => {
+    let analyzeparam ={
+        url : req.query.url,
+        features:{
+            emotion: {
 
-    return res.send({"happy":"90","sad":"10"});
+            },
+        },
+    };
+    getNLUInstance().analyze(analyzeparam)
+    .then( result => {
+            return res.send(result.result.emotion.document.emotion);
+        }
+    ).catch(err => {
+        console.log('error', err);
+    })
 });
 
 app.get("/url/sentiment", (req,res) => {
-    return res.send("url sentiment for "+req.query.url);
+    let analyzeparam ={
+        url : req.query.url,
+        features:{
+            sentiment: {
+
+            },
+        },
+    };
+    getNLUInstance().analyze(analyzeparam)
+    .then( result => {
+            return res.send(result.result.sentiment.document.label);
+        }
+    ).catch(err => {
+        console.log('error', err);
+    })
 });
 
 app.get("/text/emotion", (req,res) => {
-    return res.send({"happy":"10","sad":"90"});
+
+    let analyzeparam ={
+        text : req.query.text,
+        features:{
+            emotion: {
+
+            },
+        },
+    };
+    getNLUInstance().analyze(analyzeparam)
+    .then( result => {
+            return res.send(result.result.emotion.document.emotion);
+        }
+    ).catch(err => {
+        console.log('error', err);
+    })
 });
 
 app.get("/text/sentiment", (req,res) => {
-    return res.send("text sentiment for "+req.query.text);
+    let analyzeparam ={
+        text : req.query.text,
+        features:{
+            sentiment: {
+
+            },
+        },
+    };
+    getNLUInstance().analyze(analyzeparam)
+    .then( result => {
+            return res.send(result.result.sentiment.document.label);
+        }
+    ).catch(err => {
+        console.log('error', err);
+    })
 });
 
 let server = app.listen(8080, () => {
     console.log('Listening', server.address().port)
 })
-
-function getNLUInstance() {
-    let api_key = process.env.API_KEY;
-    let api_url = process.env.API_URL;
-
-    const NaturalLanguageUnderstandingV1 = require('ibm-watson/natural-language-understanding/v1');
-    const { IamAuthenticator } = require('ibm-watson/auth');
-
-    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
-        version: '2020-08-01',
-        authenticator: new IamAuthenticator({
-            apikey: '{apikey}',
-        }),
-        serviceUrl: '{url}',
-    });
-    return naturalLanguageUnderstanding;
-}
